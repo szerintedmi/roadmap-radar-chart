a824be4914edf339b2de3559fa24bfa598fb8aa3
bstottle <130258976+bstottle@users.noreply.github.com>
2023-04-09 09:32:11 -0400

    Remove files not needed for static rendering (#2)
    Honestly, I'm not sure what a lot of these do.  The intent is to not use
    node, so much of this shouldn't be needed.

--- copyassets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#  Copy assets to dist
-#
-
-mkdir -p dist/exampleData
-cp -R exampleData dist
-cp src/style.css dist/style.css
-cp src/svgtest.html dist/svgtest.html
--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/* Setup with jsdom but replace jsdom with svgdom in setupFilesAfterEnv so we can test svg */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["./src/jest.svgDom.ts"],
-  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
-  resolver: "ts-jest-resolver", // https://github.com/kulshekhar/ts-jest/issues/1057
-};
